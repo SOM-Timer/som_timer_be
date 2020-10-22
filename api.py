e3cdd81839ea1c069af2910c374e7af7aab66a48
Rion Hutsell <st.gish@gmail.com>
2020-10-22 15:56:54 -0600

    Initial Commit
    Co-Authored-By: Sienna Kopf <62857073+sienna-kopf@users.noreply.github.com>
    Co-Authored-By: Chandler Hulstrom <62038636+Chulstro@users.noreply.github.com>

---
+++ api.py
@@ -0,0 +1,15 @@
+import flask
+
+app = flask.Flask(__name__)
+app.config["DEBUG"] = True
+
+
+@app.route('/', methods=['GET'])
+def home():
+    return "<h1>Distant Reading Archive</h1><p>This site is a prototype API for distant reading of science fiction novels.</p>"
+
+@app.route('/jsonify/', methods=['GET'])
+def jsonify():
+    return flask.jsonify({'username': 'yxr'})
+
+app.run()
