4018db6f9f8240ad9b8cccc93b5a56b1a6a42bd0
Sienna Kopf <princess.kopf@gmail.com>
2020-10-22 19:55:13 -0600

    Add Timers table and connect som_timer db
    Co-Authored-By: Dorion <60492885+sciencefixion@users.noreply.github.com>
    Co-Authored-By: Chandler Hulstrom <62038636+Chulstro@users.noreply.github.com>

--- api.py
+++ api.py
@@ -1,8 +1,26 @@
 import flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:postgres@localhost:5432/som_timer"
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
+class TimersModel(db.Model):
+    __tablename__ = 'timers'
+
+    id = db.Column(db.Integer, primary_key=True)
+    work_interval = db.Column(db.String())
+    rest_interval = db.Column(db.String())
+
+    def __init__(self, work_interval, rest_interval):
+        self.work_interval = work_interval
+        self.rest_interval = rest_interval
+
+    def __repr__(self):
+        return f"<Timer {self.id}>"
 
 @app.route('/', methods=['GET'])
 def home():
