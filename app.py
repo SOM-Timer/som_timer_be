8c005f07627d87400483a8ad6b8dd1f3d72a8244
Chandler Hulstrom <chulstro11@gmail.com>
2020-10-27 15:48:48 -0600

    Made tests relevant to our timer endpoints

--- app.py
+++ app.py
@@ -10,15 +10,13 @@ db = SQLAlchemy(app)
 
 @app.cli.command('db_seed')
 def db_seed():
-    exercise1 = Exercise(url='https://www.youtube.com/watch?v=dsmfIAyiois', duration='5:00', category='SOMATIC')
+    exercise1 = Exercise(url='https://www.youtube.com/watch?v=W5wqniA4MMc&ab_channel=EssentialSomatics', duration='10:00', category='MEDITATION')
     exercise2 = Exercise(url='https://www.youtube.com/watch?v=_0xZ3iOswYM', duration='10:00', category='MOVEMENT')
 
     timer1 = Timer(work_interval='25:00', rest_interval='5:00')
-    timer2 = Timer(work_interval='45:00', rest_interval='15:00')
 
     db.session.add(exercise1)
     db.session.add(exercise2)
     db.session.add(timer1)
-    db.session.add(timer2)
     db.session.commit()
     print('Database Seeded!')
--- tests/test_timers.py
+++ tests/test_timers.py
@@ -17,45 +17,41 @@ class TestTimers(unittest.TestCase):
             db.session.remove()
             db.drop_all()
 
-    def test_get_all_timers(self):
+    def test_get_one_timer(self):
         timer1 = Timer(work_interval='25:00', rest_interval='5:00')
-        timer2 = Timer(work_interval='45:00', rest_interval='15:00')
         with self.app.app_context():
-            db.session.add(timer1)
-            db.session.add(timer2)
-            db.session.commit()
+                db.session.add(timer1)
+                db.session.commit()
 
-        # create a user
         response = self.test_app.get(
-            '/api/timers',
+            '/api/timers/1',
             follow_redirects=True
         )
 
-        # Assert response is 200 OK.
         self.assertEquals(response.status, "200 OK")
         payload = json.loads(response.data)
-        self.assertEquals(payload['count'], 2)
-        self.assertEquals(payload['timers'][0]['work_interval'], '25:00')
-        self.assertEquals(payload['timers'][0]['rest_interval'], '5:00')
-        self.assertEquals(payload['timers'][1]['work_interval'], '45:00')
-        self.assertEquals(payload['timers'][1]['rest_interval'], '15:00')
+        self.assertEquals(payload['work_interval'], "25:00")
+        self.assertEquals(payload['rest_interval'], '5:00')
 
-    def test_create_timer(self):
-        response = self.test_app.post(
-            '/api/timers',
+    def test_update_timer(self):
+        timer1 = Timer(work_interval='25:00', rest_interval='5:00')
+        with self.app.app_context():
+                db.session.add(timer1)
+                db.session.commit()
+
+        response = self.test_app.put(
+            '/api/timers/1',
             json={
-                'work_interval': '30:00',
-                'rest_interval': '10:00'
+                "work_interval": "30:00",
+                "rest_interval": "7:00"
             },
             follow_redirects=True
         )
 
-        # Assert response is 200 OK.
         self.assertEquals(response.status, "200 OK")
         payload = json.loads(response.data)
         self.assertEquals(payload['work_interval'], "30:00")
-        self.assertEquals(payload['rest_interval'], '10:00')
-
+        self.assertEquals(payload['rest_interval'], '7:00')
 
 if __name__ == "__main__":
     unittest.main()
