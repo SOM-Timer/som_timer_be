c6effffd724b16ad8914ba41fad79825d1abc75a
Sienna Kopf <princess.kopf@gmail.com>
2020-11-01 14:07:49 -0700

    add test for root asserting 200 status

--- tests/test_documentation.py
+++ tests/test_documentation.py
@@ -17,20 +17,13 @@ class TestDocumentation(unittest.TestCase):
             db.drop_all()
 
     def test_documentation_page(self):
-        timer1 = Timer(work_interval='25:00', rest_interval='5:00')
-        with self.app.app_context():
-                db.session.add(timer1)
-                db.session.commit()
 
         response = self.test_app.get(
-            '/api/timers/1',
+            '/',
             follow_redirects=True
         )
 
         self.assertEquals(response.status, "200 OK")
-        payload = json.loads(response.data)
-        self.assertEquals(payload['work_interval'], "25:00")
-        self.assertEquals(payload['rest_interval'], '5:00')
 
 if __name__ == "__main__":
     unittest.main()
