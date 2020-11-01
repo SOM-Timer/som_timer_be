926e85931fe0f454f9e66fda49ba9558ebdfbe21
Sienna Kopf <princess.kopf@gmail.com>
2020-10-31 20:14:25 -0600

    add sound column to timers model
    Co-Authored-By: Aaron Burris-DeBoskey <61372592+Abdeboskey@users.noreply.github.com>

--- application/models/timer.py
+++ application/models/timer.py
@@ -6,10 +6,12 @@ class Timer(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     work_interval = db.Column(db.String())
     rest_interval = db.Column(db.String())
+    sound = db.Column(db.String())
 
-    def __init__(self, work_interval, rest_interval):
+    def __init__(self, work_interval, rest_interval, sound):
         self.work_interval = work_interval
         self.rest_interval = rest_interval
+        self.sound = sound 
 
     def __repr__(self):
         return f"<Timer {self.id}>"
