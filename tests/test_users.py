182dc80fd692735bd09ad54641caf42a3e87b3fa
Sienna Kopf <princess.kopf@gmail.com>
2020-11-22 18:56:45 -0800

    Failed attempt at testing chain deletion.

--- tests/test_users.py
+++ tests/test_users.py
@@ -2,7 +2,8 @@ import unittest
 import json
 from application import create_app, db
 from application.models.user import User
-
+from application.models.rest import Rest
+from application.models.timer import Timer
 
 class TestUsers(unittest.TestCase):
     def setUp(self):
@@ -98,6 +99,24 @@ class TestUsers(unittest.TestCase):
         payload = json.loads(response.data)
         self.assertEquals(payload['user_name'], "Rachel Williams")
         self.assertEquals(payload['token'], "token3")
+
+    ## Attempt at testing chain deletion
+    # def test_user_rest_timer_chain_deletion(self):
+    #     user1 = User(user_name="Princess.Kopf", token="token1")
+    #     user2 = User(user_name="ABD", token="token2")
+    #     rest1 = Rest(mood_rating_1 = 2, mood_rating_2 = 4, rest_interval='5:00', focus_interval='30:00', content_selected='SOMATIC', user_id=user1.id)
+    #     rest2 = Rest(mood_rating_1 = 4, mood_rating_2 = 5, rest_interval='7:00', focus_interval = '25:00', content_selected='MOVEMENT', user_id=user1.id)
+    #     timer1 = Timer(work_interval='25:00', rest_interval='5:00', sound='chordCliff', mood=False, user_id=user1.id)
+    #
+    #     with self.app.app_context():
+    #         db.session.add(user1)
+    #         db.session.add(user2)
+    #         db.session.add(rest1)
+    #         db.session.add(rest2)
+    #         db.session.add(timer1)
+    #         db.session.commit()
+    #         db.session.delete(user1)
+
 
 if __name__ == "__main__":
     unittest.main()
