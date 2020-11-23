4ecbb174648d21e32cb0ecd1f2f5d4eca280d440
Sienna Kopf <princess.kopf@gmail.com>
2020-11-22 17:56:46 -0800

    Update timer model with User foreign key

--- application/models/timer.py
+++ application/models/timer.py
@@ -8,12 +8,18 @@ class Timer(db.Model):
     rest_interval = db.Column(db.String())
     sound = db.Column(db.String())
     mood = db.Column(db.Boolean())
+    user_id = db.Column(
+        db.Integer,
+        db.ForeignKey('users.id'),
+        nullable=False
+    )
 
-    def __init__(self, work_interval, rest_interval, sound, mood):
+    def __init__(self, work_interval, rest_interval, sound, mood, user_id):
         self.work_interval = work_interval
         self.rest_interval = rest_interval
         self.sound = sound
         self.mood = mood
+        self.user_id = user_id
 
     def __repr__(self):
         return f"<Timer {self.id}>"
